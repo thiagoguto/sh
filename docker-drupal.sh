5f323a5dcedf8fe99a952e91946ded4eb25d4afc
Thiago Augusto Gonçalves <thiagoaugusto31@gmail.com>
2018-09-28 18:20:53 -0300

    Update docker-drupal.sh

--- docker-drupal.sh
+++ docker-drupal.sh
@@ -1,1 +1,13 @@
-
+#!/bin/bash
+echo "Nome do Container"
+read NOME
+echo "Porta"
+read PORTA
+echo "Servidor MYSQL"
+read MYSQL
+echo "Versao do drupal (:8.6.1)"
+read VERSAO
+docker run --name $NOME -p $PORTA:80 \
+--link $MYSQL:mysql \
+-v /Users/admin/Dev/drupal/temas/$NOME:/var/www/html/themes/custom \
+-d drupal$VERSAO
